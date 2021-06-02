c08e0a570c4a9821269deef2804a607da8a0fb45
SwalveSam <samantha.swalve@gmail.com>
2021-06-02 19:44:07 +0000

    added feature for users to login

--- model.py
+++ model.py
@@ -1,10 +1,11 @@
 """ Models for capstone project"""
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     """ A user"""
     __tablename__ = 'users'
 
@@ -12,12 +13,23 @@ class User(db.Model):
     email = db.Column(db.String, unique=True)
     password = db.Column(db.String)
 
+    def get_id(self):
+        """ Override UserMixin.get_id."""
+        return str(self.user_id)
+
     def __repr__(self):
         """Show info about user"""
         return f'<User_id = {self.user_id} Email = {self.email}>'
 
 
-def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
+
+
+
+
+
+
+
+def connect_to_db(flask_app, db_uri='postgresql:///users', echo=True):
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     flask_app.config['SQLALCHEMY_ECHO'] = echo
     flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -26,7 +38,7 @@ def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
     db.init_app(flask_app)
 
     print('Connected to the db!')
-    
+
 
 if __name__ == '__main__':
     from server import app
--- server.py
+++ server.py
@@ -1,64 +1,88 @@
-""" Server for Capstone Project"""
+"""Server for Capstone Project"""
 
 from flask import Flask
+#Allow users to log in/manage login information
+from flask_login import LoginManager, login_user, login_required
 
 from flask import (Flask, render_template, request, flash, session,
                    redirect)
 import jinja2
 
-from model import connect_to_db
+from model import connect_to_db, User
 
 app = Flask(__name__)
 app.secret_key = "secret-key"
-
 #app.jinja_env.undefined = StrictUndefined
+
+#creates LoginManager and attaches to Flask app instance
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 
 @app.route('/')
 def homepage():
     """View Homepage"""
 
-    render_template('homepage.html')
+    return render_template('homepage.html')
 
-@app.route('/login', methods=['POST'])
+# #callback for login_manager.user_loader
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+@app.route("/login", methods=["GET","POST"])
 def login():
     """Allows users to login """
-    # if returning user submit button takes to user homepage 
-    #     render_template('users.html')
-    # elif if information entered incorrectly have user try again
+    email = request.form.get("email")
+    password = request.form.get("password")
 
-    # else need to create a user 
+    user = User.query.filter_by(email=email).first()
+
+    if password == user.password:
+        # Call flask_login.login_user to login a user
+        login_user(user)
+
+        flash("YAY! You're Logged in!")
+    
+        return redirect("/users")
+    
+    flash("Email or Password invalid. Please try again.")
+    
+    return redirect("/")
+
+@app.route("/register", methods=["POST"])
+
  
 
 
+@app.route('/users')
+def users():
+    """ View users profile """
 
-   
+    return render_template('users.html')
 
+@app.route('/homes')
+def homes():
+    """ View and search for homes"""
 
-# @app.route('users')
-# def users():
-#     """ View users profile """
+    return render_template('homes.html')
 
-#     render_template('users.html')
-
-# @app.route('homes')
-# def homes():
-#     """ View and search for homes"""
-
-#     render_template('homes.html')
-
-# @app.route('businesses')
+# @app.route('/businesses')
 # def businesses():
 #     """ View businesses"""
 
-#     render_template('businesses.html')
+#     return render_template('/businesses.html')
 
-# @app.route('maps')
+# @app.route('/maps')
 # def maps():
 #     """View homes and businesses on map"""
 
-#     render_template('maps.html')
-
+#     return render_template('/maps.html')
 
 if __name__ == "__main__":
     connect_to_db(app)
     app.run(debug=True, host='0.0.0.0')
+
+
+
+# Hi, Im in Malala C, I'm getting a NameError User not defined 
